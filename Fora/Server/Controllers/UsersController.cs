4ce873a70931bdcaed9f73bbe872bc67867da680
jonathandepartment <68937234+jonathandepartment@users.noreply.github.com>
2022-04-04 14:38:03 +0200

    Added UsersController

---
+++ Fora/Server/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fora.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public UsersController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateUser(UserModel user)
+        {
+            
+            return Created("", user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SignIn()
+        {
+            // sign in user
+            // return token
+            // return unauthorized
+            return Unauthorized();
+        }
+    }
+}
