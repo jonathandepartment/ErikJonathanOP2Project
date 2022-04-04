20535c57c3daffcf715ac52c5f85af872bef47d8
Noobieswede <erik.enander95@gmail.com>
2022-04-04 13:45:41 +0200

    Crated InterestsController

---
+++ Fora/Server/Controllers/InterestsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fora.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterestsController : ControllerBase
+    {
+    }
+}
