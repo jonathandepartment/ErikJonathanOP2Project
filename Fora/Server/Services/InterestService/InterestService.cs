0581296b34018e0099ab182b12d1b7f6fe2d2c77
Noobieswede <erik.enander95@gmail.com>
2022-04-06 15:16:00 +0200

    Added GetUserInterests

--- Fora/Server/Services/InterestService/InterestService.cs
+++ Fora/Server/Services/InterestService/InterestService.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace Fora.Server.Services.InterestService
+﻿namespace Fora.Server.Services.InterestService
 {
     public class InterestService : IInterestService
     {
@@ -27,10 +25,32 @@ namespace Fora.Server.Services.InterestService
             return null;
         }
 
-        public Task<List<InterestModel>> GetUserIneterests(int id)
+        public async Task<List<UserInterestModel>> GetUserInterests(int id)
         {
-            throw new NotImplementedException();
+            // Kontakta databasen och hämta alla intressen som en specifik användare har
+
+            // *********************************************
+            // ERROR!! A possible object cycle was detected.
+            // *********************************************
+            var users = await _context.Users.Include(i => i.UserInterests).ToListAsync();
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user != null)
+            {
+                return user.UserInterests;
+
+            }
+            return null;
         }
+
+        // Kontakta databasen och hämta alla intressen som en specifik användare har
+        //var userInterestsList = await _context.Users.Include(i => i.Interests).ToListAsync();
+        //    if (userInterestsList != null)
+        //    {
+        //        return null;
+        //    }
+        //    return null;
+
+
 
         public Task<List<InterestModel>> PostNewInterest()
         {
