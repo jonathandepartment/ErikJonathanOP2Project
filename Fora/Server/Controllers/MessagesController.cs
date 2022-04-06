39dbd6e27e2812d79c2b7b14544d6cae1e21b9ce
jonathandepartment <68937234+jonathandepartment@users.noreply.github.com>
2022-04-06 14:31:42 +0200

    Added change and remove message

---
+++ Fora/Server/Controllers/MessagesController.cs
@@ -0,0 +1,43 @@
+﻿using Fora.Server.Services.MessageService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fora.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+
+        public MessagesController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> ChangeMessage(int id,string newMessage)
+        {
+            // get user from request
+            //var userName = User.Identity.Name;
+            var result = await _messageService.EditMessage(id,newMessage);
+            if (result != null)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> RemoveMessage(int id)
+        {
+            // get user from request
+            //var userName = User.Identity.Name;
+            var result = await _messageService.DeleteMessage(id);
+            if (result)
+            {
+                return NoContent();
+            }
+            return BadRequest();
+        }
+
+    }
+}
