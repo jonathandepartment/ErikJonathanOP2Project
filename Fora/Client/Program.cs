f74f1eef6466099f399ec52519e7069f1f57c3e2
jonathandepartment <68937234+jonathandepartment@users.noreply.github.com>
2022-04-07 15:10:50 +0200

    Implemented State and Login component

--- Fora/Client/Program.cs
+++ Fora/Client/Program.cs
@@ -1,3 +1,4 @@
+global using Microsoft.AspNetCore.Components.Authorization;
 using Fora.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
