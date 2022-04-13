27e0effca02addd04d55151327802c7bffccb836
Noobieswede <erik.enander95@gmail.com>
2022-04-13 15:50:02 +0200

    AddedThreadmessages

---
+++ Fora/Client/Services/MessageService/IMessageService.cs
@@ -0,0 +1,9 @@
+﻿using Fora.Shared.ViewModels;
+
+namespace Fora.Client.Services.MessageService
+{
+    public interface IMessageService
+    {
+        Task<List<MessageViewModel>> GetThreadMessages(int id);
+    }
+}
