95cd54561508b5795a573d84d0167dbcf2e343e8
jonathandepartment <68937234+jonathandepartment@users.noreply.github.com>
2022-04-13 09:19:13 +0200

    Updated message model

---
+++ Fora/Server/Migrations/20220413071146_ChangedMessageModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fora.Server.Migrations
+{
+    public partial class ChangedMessageModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Messages",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Messages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Edited",
+                table: "Messages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "Edited",
+                table: "Messages");
+        }
+    }
+}
