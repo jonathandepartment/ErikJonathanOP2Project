05aca2660ce1fc8c089df084e784d089f1ac3474
jonathandepartment <68937234+jonathandepartment@users.noreply.github.com>
2022-04-13 11:00:28 +0200

    Added services, modified pickinterest page

--- Fora/Shared/DTO/AddInitialInterests.cs
+++ Fora/Shared/DTO/AddInitialInterests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Fora.Shared.DTO
 {
     public class AddInitialInterests
     {
+        [Required, MinLength(1), MaxLength(5)]
         public List<int> InterestIds { get; set; }
     }
 }
